e3aa7c00a20a45050ee4f3b78fe86d7e07919cd8
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-02-19 17:47:06 +0500

    day 39/100 done

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,15 +7,94 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomText: View {
+    let text: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating new custom Text")
+        self.text = text
+    }
+}
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    
+    @State private var decodedJSON: String = ""
+    
+    let layout = [
+        /*GridItem(.fixed(80)),
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80)),
+        GridItem(.fixed(80))*/
+        GridItem(.adaptive(minimum: 80))
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            Button("Decode JSON") {
+                let input = """
+                {
+                    "name": "Izaan Saleem",
+                    "address": {
+                        "street": "55 abc ave",
+                        "city": "Karachi"
+                    }
+                }
+                """
+                
+                let data = Data(input.utf8)
+                let decoder = JSONDecoder()
+                
+                if let user = try? decoder.decode(User.self, from: data) {
+                    print(user.address.street)
+                    decodedJSON = user.name
+                }
+            }
+            if decodedJSON != "" {
+                Text("Name from Decoded JSON => \(decodedJSON)")
+            }
+            List(0..<6) { row in
+                NavigationLink("Row: \(row)") {
+                    Text("Detail \(row)")
+                }
+            }
+            ScrollView {
+                LazyVGrid(columns: layout) {
+                    ForEach(0..<200) {
+                        Text("Item: \($0)")
+                    }
+                }
+                Image(.example1)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { size, axis in
+                        size * 0.95
+                    }
+                VStack(spacing: 10) {
+                    ForEach(0..<2) {
+                        CustomText(text: "Item: \($0)")
+                            .font(.title2)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                
+                
+            }
+            .navigationTitle("SwiftUI")
         }
-        .padding()
     }
 }
 
