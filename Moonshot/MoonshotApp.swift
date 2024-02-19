086d872264f28a860978301e52e7b9158c825a16
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2024-02-19 14:48:35 +0500

    first commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Izaan Saleem on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
